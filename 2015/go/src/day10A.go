8c2a6bf99231566b45758bb5001bf99a0257e30d
Mladen Kliska <mladenkliska@Mladens-MacBook-Pro.local>
2020-12-10 16:30:26 -0500

    Added day 10 2015

---
+++ 2015/go/src/day10A.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+func parseNumber(num string) string {
+	val := ""
+
+	tmp := ""
+	counter := 0
+
+	for _, n := range num {
+		if tmp != string(n) {
+
+			if counter > 0 {
+				val = val + strconv.Itoa(counter) + tmp
+			}
+
+			counter = 1
+			tmp = string(n)
+		} else {
+			counter = counter + 1
+		}
+	}
+
+	if counter > 0 {
+		val = val + strconv.Itoa(counter) + tmp
+	}
+
+	return val
+}
+
+func main() {
+	input := readFile("day10.dat")
+
+	fmt.Println(input)
+
+	counter := 1
+	num := input
+
+	for counter <= 40 {
+		num = parseNumber(num)
+
+		counter++
+	}
+
+	fmt.Println(len(num))
+}
