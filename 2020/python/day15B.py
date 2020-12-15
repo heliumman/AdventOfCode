7a6c39f311ab3fffd9a5e3065cae87531897ee56
Mladen Kliska <mladenkliska@Mladens-MacBook-Pro.local>
2020-12-15 11:34:19 -0500

    Added day 15 2020

---
+++ 2020/python/day15B.py
@@ -0,0 +1,31 @@
+import common
+
+
+def main() :
+    dat = common.readFile('day15.dat')
+    nums = []
+    last_seen = {}
+    
+    for d in dat[0].split(','):
+        nums.append(int(d))
+        last_seen[int(d)] = (dat[0].split(',').index(d) + 1, -1)
+    
+    nums = play_game(nums, last_seen)
+
+    print(nums[-1])
+
+def play_game(nums, last_seen):
+    while len(nums) < 30000000:
+        last_num = nums[-1]
+        last_said  = last_seen.get(last_num, (-1, -1))
+
+        last_seen[last_num] = (len(nums), last_said[0])
+        if last_seen[last_num][1] == -1:
+            nums.append(0)
+        else:
+            nums.append(last_seen[last_num][0] - last_seen[last_num][1])
+    
+    return nums
+
+if __name__ == '__main__':
+    main()
